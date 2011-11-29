8bd0bf2be81f35f0e3b445f649ad5935609e9e71
Tom Kazimiers <tom@voodoo-arts.net>
2011-11-29 16:02:28 +0100

    Comment change

--- httpdocs/stack.js
+++ httpdocs/stack.js
@@ -49,6 +49,7 @@ function Stack(
 	 */
 	var updateScaleBar = function()
 	{
+		// calculate pixels per nanometers wrt. scale
 		var meter = self.scale / resolution.x;
 		var width = 0;
 		var text = "";
